cbfbefe0c49bedca1baaf54294412bc0de128c35
joynchou <602503587@qq.com>
2017-07-11 23:13:36 +0800

    增加了一些器件的驱动
    增加了74h595，mpu9250的驱动（未完成），写了一个数字滤波器

--- HARDWARE/DEVICES/SENSOR/ANGLE/ANGLE.C
+++ HARDWARE/DEVICES/SENSOR/ANGLE/ANGLE.C
@@ -1,4 +1,5 @@
 #include "ANGLE.h"
+#include "../../../../SOFTWARE/ALGORITHM/FILTER/FILTER.H"
 #define  POTENTIOMETER
 #ifdef POTENTIOMETER
 //ADC初始化函数 p1^0
@@ -36,28 +37,6 @@ void    ADC_config(u8 pin, u8 speed)
     //ADC_PowerControl(ENABLE);
 }
 
-u16 filter(u16 *ary,u8 num,u16 MaxValue,u16 MinValue)
-{
-	int angleDataloopTemp2;
-	u16 finalData=0;
-	int maxValue = MinValue;
-	int minValue = MaxValue;
-	for (angleDataloopTemp2 = 0; angleDataloopTemp2 < num; angleDataloopTemp2++) //找出最大值和最小值
-	{
-		if (ary[angleDataloopTemp2] > maxValue) //去掉最大值最小值
-		{
-			maxValue = ary[angleDataloopTemp2];
-		}
-		if (ary[angleDataloopTemp2] < minValue) //去掉最大值最小值
-		{
-			minValue = ary[angleDataloopTemp2];
-		}
-		finalData += ary[angleDataloopTemp2];
-	}
-	finalData -= (maxValue + minValue);  //去掉最大值最小值
-	finalData /= (num-2);	 //取平均值
-	return finalData;
-}
 
 /*************************************************
 * 函数名称: float getAngle(bit whichAngle)
